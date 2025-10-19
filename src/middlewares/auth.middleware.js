3d57e1739c3e65f5dc3445993f441dcb43a56e41
Dhruv102004 <dhruvagrawals@gmail.com>
2025-10-20 03:43:35 +0530

    add refresh token endpoint

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,5 +1,4 @@
 import { ApiError } from "../utils/ApiError.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
 import { User } from "../models/user.model.js";
 
@@ -11,7 +10,7 @@ export const verifyJWT = async(req, _, next) => {
         }
     
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-        const user = await User.findById(decodedToken?.id).select("-password -refreshToken")
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
         
         if (!user) {
             throw new ApiError(401, "Invalid Access Token")
